6235d6656c5508950de4cfa75bc096ab9feadf94
alejandro ahumada ravelo <alejandro ahumada ravelo>
2016-06-21 00:01:02 -0400

    ajues login-signin

--- app/src/main/java/cl/selftourhamburger/Activity/Activity_Login_Signin.java
+++ app/src/main/java/cl/selftourhamburger/Activity/Activity_Login_Signin.java
@@ -15,12 +15,12 @@ public class Activity_Login_Signin extends AppCompatActivity {
 
     CarouselView carouselView;
 
-    int[] sampleImages = {R.drawable.cajon1, R.drawable.cajon2, R.drawable.cajon3, R.drawable.cajon4};
+    int[] sampleImages = {R.drawable.fondo1, R.drawable.fondo2, R.drawable.fondo3, R.drawable.fondo2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lyt_activity_login_signin);
+        setContentView(R.layout.lyt_activity_login_sign);
 
         carouselView = (CarouselView) findViewById(R.id.carouselView);
         carouselView.setPageCount(sampleImages.length);
